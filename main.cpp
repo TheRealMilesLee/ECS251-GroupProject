0d52d85ec98d6a64a9767dae4c353661ad129567
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-01-11 15:17:21 -0800

    Update the template

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+void parallel_execute(vector<int> incoming_job, int num_threads, bool is_dependent)
+{
+  // Create threads and execute jobs in parallel
+
+}
