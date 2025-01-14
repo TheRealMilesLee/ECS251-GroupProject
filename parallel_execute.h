e163b70a64cef8553c3eca24e2b42e52d992ca86
Hengyi Li <trdli@ucdavis.edu>
2025-01-14 15:09:16 -0800

    Update the duty

--- parallel_execute.h
+++ parallel_execute.h
@@ -12,10 +12,10 @@ public:
   void parallel_execute_matrix_multiplication(
       std::vector<std::vector<int>> &matrix1,
       std::vector<std::vector<int>> &matrix2,
-      std::vector<std::vector<int>> &result);
-  void parallel_execute_sort(std::vector<int> &v);
-  void parallel_execute_array_sum(std::vector<int> &v);
-  void parallel_execute_array_search(std::vector<int> &v, int key);
+      std::vector<std::vector<int>> &result); // Michael
+  void parallel_execute_sort(std::vector<int> &v); // Zhuosheng
+  void parallel_execute_array_sum(std::vector<int> &v); // Jason
+  void parallel_execute_array_search(std::vector<int> &v, int key); // Hengyi
 };
 
 void parallel_execute::parallel_execute_matrix_multiplication(
