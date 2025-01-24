23202081079bd7f1d82f00d1e0548c0f447ec40c
Hengyi Li <trdli@ucdavis.edu>
2025-01-24 15:52:55 -0800

    update the test shell

--- testBash.sh
+++ testBash.sh
@@ -2,7 +2,7 @@
 
 my_array=()
 
-for i in {1..3276800000}; do
+for i in {1..32768}; do
   my_array+=($RANDOM)
 done
 
@@ -10,4 +10,5 @@ done
 random_index=$((RANDOM % 32768))
 my_array=("${my_array[@]:0:$random_index}" 10240 "${my_array[@]:$random_index}")
 
-parallel_execute array_search 10240 "${my_array[@]}"
+# parallel_execute array_search 10240 "${my_array[@]}"
+parallel_execute sort "${my_array[@]}"
