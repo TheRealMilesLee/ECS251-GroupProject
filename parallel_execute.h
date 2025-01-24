4760fca419fb26f0c8e9ab4982d3cba5595868f5
Hengyi Li <trdli@ucdavis.edu>
2025-01-24 13:55:48 -0800

    UPDATE THE DOC

--- parallel_execute.h
+++ parallel_execute.h
@@ -35,15 +35,17 @@ void parallel_execute::parallel_execute_array_search(std::vector<int> &v,
   std::mutex mtx;
   std::atomic<int> foundIndex(-1);
 
-  // 获取硬件支持的线程数量
+  // Get the number of threads supported by the hardware
   unsigned int numThreads = std::thread::hardware_concurrency();
   if (numThreads == 0)
-    numThreads = 8; // 默认值
+  {
+    numThreads = 8; // Default to 8 threads
+  }
 
-  // 计算每个线程处理的区间大小
+  // Calculate the chunk size
   size_t chunkSize = v.size() / numThreads;
 
-  // 清空之前的线程
+  // Clear the threads vector
   threads.clear();
 
   for (unsigned int i = 0; i < numThreads; ++i)
@@ -56,7 +58,7 @@ void parallel_execute::parallel_execute_array_search(std::vector<int> &v,
         {
           for (size_t index = start; index < end && foundIndex == -1; ++index)
           {
-            if (searchIndex(key, index, v) != -1)
+            if (v[index] != -1)
             {
               std::lock_guard<std::mutex> lock(mtx);
               foundIndex = index;
@@ -79,13 +81,5 @@ void parallel_execute::parallel_execute_array_search(std::vector<int> &v,
   {
     std::cout << "Found key " << key << " at index " << foundIndex
               << std::endl;
-  }
-}
-
-int searchIndex(int searchNum, int currentIndex, std::vector<int> &v)
-{
-  if (v[currentIndex] == searchNum)
-  {
-    return currentIndex;
   }
 }
