f05f4809b132485b994e8554bb4d6de09c9cc918
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-02-10 18:10:53 -0800

    Update the testing script

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 CXX = clang++
-CXXFLAGS = -Oz -pg -pedantic-errors -Weverything -ltbb -Wno-poison-system-directories -Wthread-safety -Wno-c++98-compat -std=c++23 -pthread
+CXXFLAGS = -Oz -g -pedantic-errors -Weverything -ltbb -Wno-poison-system-directories -Wthread-safety -Wno-c++98-compat -std=c++23 -pthread
 
 SRC = matrix_mul.cpp parallel_matrix_mul.cpp parallel_matrix_mul_async.cpp parallel_matrix_mul_fifo.cpp parallel_matrix_mul_lifo.cpp parallel_matrix_mul_tbb.cpp parallel_matrix_mul_ppl.cpp
 OBJ = $(SRC:.cpp=.o)
 
-TARGETS = matrix_mul_single matrix_multiply_parallel parallel_matrix_mul_async parallel_matrix_mul_fifo parallel_matrix_mul_lifo parallel_matrix_mul_tbb parallel_matrix_mul_ppl
+TARGETS = matrix_mul_single matrix_multiply_parallel matrix_mul_async matrix_mul_fifo matrix_mul_lifo matrix_mul_tbb matrix_mul_ppl
 
 all: $(TARGETS)
 
@@ -29,11 +29,8 @@ parallel_matrix_mul_tbb: parallel_matrix_mul_tbb.o
 parallel_matrix_mul_ppl: parallel_matrix_mul_ppl.o
 	$(CXX) $(CXXFLAGS) -o $@ $^
 
-
-
-
 %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c $< -o $@
 
 clean:
-	rm -f $(OBJ) $(TARGETS) Results/gmon_matrix_mul.out Results/gmon_parallel_matrix_mul.out
+	rm -f $(OBJ) $(TARGETS)
--- perfRecord.sh
+++ perfRecord.sh
@@ -7,31 +7,66 @@ timestamp=$(date +"%Y%m%d%H%M%S")
 
 # Start the perf stat for the matrix_mul_single
 perf stat ./matrix_mul_single 2>Results/perfStats_single_$timestamp.txt
-mv gmon.out Results/gmon_matrix_mul.out
 
 # Start the perf stat for the matrix_multiply_parallel
 perf stat ./matrix_multiply_parallel 2>Results/perfStats_parallel_$timestamp.txt
-mv gmon.out Results/gmon_parallel_matrix_mul.out
+
+# Start the perf stat for the matrix_mul_async
+perf stat ./matrix_mul_async 2>Results/perfStats_async_$timestamp.txt
+
+# Start the perf stat for the matrix_mul_fifo
+perf stat ./matrix_mul_fifo 2>Results/perfStats_fifo_$timestamp.txt
+
+# Start the perf stat for the matrix_mul_lifo
+perf stat ./matrix_mul_lifo 2>Results/perfStats_lifo_$timestamp.txt
+
+# Start the perf stat for the matrix_mul_tbb
+perf stat ./matrix_mul_tbb 2>Results/perfStats_tbb_$timestamp.txt
+
+# Start the perf stat for the matrix_mul_ppl
+perf stat ./matrix_mul_ppl 2>Results/perfStats_ppl_$timestamp.txt
 
 # Start the perf record for the matrix_mul_single, record every possible thing save the data in perf-single.data
 perf record -o Results/perf-single.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_mul_single
-mv gmon.out Results/gmon_matrix_mul.out
 
 # Start the perf record for the matrix_multiply_parallel, record every possible thing save the data in perf-parallel.data
 perf record -o Results/perf-parallel.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_multiply_parallel
-mv gmon.out Results/gmon_parallel_matrix_mul.out
+
+# Stat the perf record for the matrix_mul_async, record every possible thing save the data in perf-async.data
+perf record -o Results/perf-async.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_mul_async
+
+# Stat the perf record for the matrix_mul_fifo, record every possible thing save the data in perf-fifo.data
+perf record -o Results/perf-fifo.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_mul_fifo
+
+# Stat the perf record for the matrix_mul_lifo, record every possible thing save the data in perf-lifo.data
+perf record -o Results/perf-lifo.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_mul_lifo
+
+# Stat the perf record for the matrix_mul_tbb, record every possible thing save the data in perf-tbb.data
+perf record -o Results/perf-tbb.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_mul_tbb
+
+# Stat the perf record for the matrix_mul_ppl, record every possible thing save the data in perf-ppl.data
+perf record -o Results/perf-ppl.data -a -g -e cycles,instructions,cache-references,cache-misses,branch-instructions,branch-misses,context-switches,cpu-migrations,faults,task-clock -- ./matrix_mul_ppl
+
+# Generate the report for the matrix_mul_async
+perf report -i Results/perf-async.data --stdio >Results/perfReport_async_$timestamp.txt
+
+# Generate the report for the matrix_mul_fifo
+perf report -i Results/perf-fifo.data --stdio >Results/perfReport_fifo_$timestamp.txt
+
+# Generate the report for the matrix_mul_lifo
+perf report -i Results/perf-lifo.data --stdio >Results/perfReport_lifo_$timestamp.txt
+
+# Generate the report for the matrix_mul_tbb
+perf report -i Results/perf-tbb.data --stdio >Results/perfReport_tbb_$timestamp.txt
+
+# Generate the report for the matrix_mul_ppl
+perf report -i Results/perf-ppl.data --stdio >Results/perfReport_ppl_$timestamp.txt
 
 # Generate the report for the matrix_mul_single
 perf report -i Results/perf-single.data --stdio >Results/perfReport_single_$timestamp.txt
 
 # Generate the report for the matrix_multiply_parallel
 perf report -i Results/perf-parallel.data --stdio >Results/perfReport_parallel_$timestamp.txt
-
-# using gprof to generate the profile for the matrix_mul_single
-gprof matrix_mul_single Results/gmon_matrix_mul.out >Results/gprof_single_$timestamp.txt
-
-# using gprof to generate the profile for the matrix_multiply_parallel
-gprof matrix_multiply_parallel Results/gmon_parallel_matrix_mul.out >Results/gprof_parallel_$timestamp.txt
 
 # clean the files
 make clean
