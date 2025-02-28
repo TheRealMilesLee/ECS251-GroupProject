8befe058ca639f981c5b56127bd680f239b61154
Hengyi Li <trdli@ucdavis.edu>
2025-02-27 22:49:57 -0800

    Update the scripts

--- perfRecord-intel.sh
+++ perfRecord-intel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -rf Results/ Results.7z
+rm -rf Results_Intel/ Results_Intel.7z
 make clean
 
-mkdir Results
+mkdir Results_Intel
 make -j$(nproc) >/dev/null
 
 # Collect all the events we want to monitor (Intel compatible)
@@ -14,7 +14,7 @@ run_perf() {
   cmd=$2
   timestamp=$(date +"%Y%m%d%H%M%S") # 每次运行生成不同时间戳
   echo "Running $name test..."
-  sudo perf stat -e $events -- $cmd 2>Results/perfStats_${name}_$timestamp.txt
+  sudo perf stat -e $events -- $cmd 2>Results_Intel/perfStats_${name}_$timestamp.txt
 }
 
 # Run perf on all approaches
@@ -28,22 +28,22 @@ run_perf "tbb" ./matrix_mul_tbb
 run_perf "openBLAS" ./matrix_mul_blas
 run_perf "openMP" ./matrix_mul_openmp
 
-# Run test on all results
-echo "Validating the results"
+# Run test on all Results_Intel
+echo "Validating the Results_Intel"
 for variant in async fifo lifo tbb standard openBLAS openMP; do
   if [ "$variant" != "openBLAS" ]; then
-    diff -u <(sort matrix_mul_single.txt) <(sort parallel_matrix_mul_${variant}.txt) | grep -E "^[+|-]" | grep -v "+++" >Results/diff_parallel_${variant}_$(date +"%Y%m%d%H%M%S").txt
-    if [ $(wc -c <Results/diff_parallel_${variant}_*.txt) -eq 0 ]; then
+    diff -u <(sort matrix_mul_single.txt) <(sort parallel_matrix_mul_${variant}.txt) | grep -E "^[+|-]" | grep -v "+++" >Results_Intel/diff_parallel_${variant}_$(date +"%Y%m%d%H%M%S").txt
+    if [ $(wc -c <Results_Intel/diff_parallel_${variant}_*.txt) -eq 0 ]; then
       echo -e "\e[32mValidation Passed for $variant\e[0m"
-      rm -rf Results/diff_parallel_${variant}_*.txt
+      rm -rf Results_Intel/diff_parallel_${variant}_*.txt
     else
       echo -e "\e[31mValidation Failed for $variant\e[0m"
     fi
   else
-    diff -u <(sort matrix_mul_double.txt) <(sort parallel_matrix_mul_${variant}.txt) | grep -E "^[+|-]" | grep -v "+++" >Results/diff_${variant}_$(date +"%Y%m%d%H%M%S").txt
-    if [ $(wc -c <Results/diff_${variant}_*.txt) -eq 0 ]; then
+    diff -u <(sort matrix_mul_double.txt) <(sort parallel_matrix_mul_${variant}.txt) | grep -E "^[+|-]" | grep -v "+++" >Results_Intel/diff_${variant}_$(date +"%Y%m%d%H%M%S").txt
+    if [ $(wc -c <Results_Intel/diff_${variant}_*.txt) -eq 0 ]; then
       echo -e "\e[32mValidation Passed for $variant\e[0m"
-      rm -rf Results/diff_${variant}_*.txt
+      rm -rf Results_Intel/diff_${variant}_*.txt
     else
       echo -e "\e[31mValidation Failed for $variant\e[0m"
     fi
@@ -54,5 +54,5 @@ done
 make clean
 rm -rf matrix_mul_*.txt parallel_matrix_mul_*.txt
 
-# Zip results
-sudo 7z a -t7z -mx=9 -mmt=on -m0=lzma2 -md=1024m -ms=on Results.7z Results/
+# Zip Results_Intel
+sudo 7z a -t7z -mx=9 -mmt=on -m0=lzma2 -md=1024m -ms=on Results_Intel.7z Results_Intel/
